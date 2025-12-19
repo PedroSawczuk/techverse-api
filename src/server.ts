33d6083ff42d9bb32e69c8d23e7af4dab1dc02ef
PedroSawczuk <pedrosawczuk53@gmail.com>
2025-12-18 22:47:09 -0400

    update server listen configuration

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import { app } from "@/app";
+import { app } from "./app";
 
 app.get('/', () => {
     return 'Hello World!'
 })
 
-app.listen({ port: 3333 })
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log(`🔥 API rodando em http://localhost:3333`)
+})
